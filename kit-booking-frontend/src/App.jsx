19b5e1112c215180e9757f16ccd60a2885618945
oluwatobi16 <oluwatobi.afolabi@yahoo.co.uk>
2025-10-03 15:59:39 +0100

    Renamed Pages to be more intuitive added, Return Page and proper home page to select between returns page and bookings page.

--- kit-booking-frontend/src/App.jsx
+++ kit-booking-frontend/src/App.jsx
@@ -1,17 +1,21 @@
 import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
-import BookingPage from "./pages/BookingPage.jsx"; // we'll make this
+import BookingPage from "./pages/BookingPage.jsx";
 import ConfirmationPage from "./pages/ConfirmationPage.jsx";
+import FinishBookingPage from "./pages/FinishBookingPage.jsx";
 import HomePage from "./pages/HomePage.jsx";
-import LandingPage from "./pages/LandingPage.jsx";
+import KitSelectionPage from "./pages/KitSelectionPage.jsx";
+import ReturnPage from "./pages/ReturnPage.jsx";
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<HomePage />} />
+        <Route path="/kitSelection" element={<KitSelectionPage />} />
         <Route path="/booking" element={<BookingPage />} />
-        <Route path="/finish" element={<LandingPage />} />
+        <Route path="/finish" element={<FinishBookingPage />} />
         <Route path="/confirmed" element={<ConfirmationPage />} />
+        <Route path="/return" element={<ReturnPage />} />
       </Routes>
     </Router>
   );
