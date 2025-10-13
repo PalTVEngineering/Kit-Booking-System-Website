cfdb09f693f09cf0d91962f97f75bb2153e4dabc
oluwatobi16 <oluwatobi.afolabi@yahoo.co.uk>
2025-10-13 23:22:25 +0100

    Removed debugging line

--- kit-booking-frontend/src/services/api.js
+++ kit-booking-frontend/src/services/api.js
@@ -1,6 +1,5 @@
 import axios from "axios";
 
-console.log("VITE_API_BASE_URL being used:", import.meta.env.VITE_API_BASE_URL);
 const API = axios.create({
   baseURL: import.meta.env.VITE_API_BASE_URL || "/api",
 });
